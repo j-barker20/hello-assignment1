336d773688daad849926fbcd518cfb825d989bba
Joshua Barker <69769853+j-barker20@users.noreply.github.com>
2020-09-03 16:43:01 -0500

    Add files via upload

---
+++ helloName.c
@@ -0,0 +1,13 @@
+/**
+ * authors: Joshua Barker, Erica Fenn
+ * Date: 2020/09/04
+ * 
+ * 
+ **/
+
+ #include <stdio.h>
+ 
+ int main(int argc, char **argv) {
+     printf("Joshua Barker\n");
+     
+ }
